25180b65b66509a6008f4b964f31f8bb01d1530c
patryk913 <patrykjss1993@gmail.com>
2016-09-11 23:34:37 +1000

    Summery Page

--- LAPIZZA2/SQLite-AppSample/SQLiteSamplePage.cs
+++ LAPIZZA2/SQLite-AppSample/SQLiteSamplePage.cs
@@ -1,7 +1,9 @@
 ﻿using SQLite;
+using System;
 using Xamarin.Forms;
 using XamarinForms.SQLite;
 using XamarinForms.SQLite.SQLite;
+using System.Threading.Tasks;
 
 namespace LAPIZZA2
 {
@@ -16,30 +18,10 @@ namespace LAPIZZA2
 
             _sqLiteConnection.CreateTable<TodoItem>();
 
-            // ADD
-            _sqLiteConnection.Insert(new TodoItem
-            {
-                Text = "Tutorial about SQLite",
-                Done = false,
-            });
-
-            // UPDATE
-            _sqLiteConnection.Update(new TodoItem
-            {
-                ID = 1,
-                Text = "Creating a tutorial about SQLite",
-                Done = true,
-            });
-
-            _sqLiteConnection.Insert(new TodoItem
-            {
-                Text = "Meeting Friends at 11 AM",
-                Done = true,
-            });
-
-            // DELETE
-            _sqLiteConnection.Delete<TodoItem>(2);
+           
         }
+
+        public Task Navigation { get; private set; }
 
         /// <summary>
         /// Gets a ContentPage that contains the items saved in the SQLite database.
@@ -50,58 +32,165 @@ namespace LAPIZZA2
             var picker = new Picker
             {
                 Title = "Pizza Base",
-                IsVisible = true //<= Is on the page but doesnt use space
+                IsVisible = true 
             };
             picker.Items.Add("Itallian");
             picker.Items.Add("Thick pan");
 
-
-            var PizzaName = new Entry
+            var picker2 = new Picker
             {
-                Placeholder = "Pizzan Type",
-                WidthRequest = Device.OnPlatform<double>(300, 300, 260)
+                Title = "Topping No 1",
+                IsVisible = true 
             };
+            picker2.Items.Add("Pepperoni");
+            picker2.Items.Add("Ham");
+            picker2.Items.Add("Mushroom");
+            picker2.Items.Add("Chillies");
+            picker2.Items.Add("Toffu");
+            picker2.Items.Add("Capsicum");
+            picker2.Items.Add("Veg");
+            picker2.Items.Add("Pumpkin");
+            picker2.Items.Add("None");
 
-            var PizzaTopping = new  Entry
+
+            var picker3 = new Picker
             {
-                Placeholder = "Topping",
-                WidthRequest = Device.OnPlatform<double>(300, 300, 260)
+                Title = "Topping #2",
+                IsVisible = true 
             };
+            picker3.Items.Add("Pepperoni");
+            picker3.Items.Add("Ham");
+            picker3.Items.Add("Mushroom");
+            picker3.Items.Add("Chillies");
+            picker3.Items.Add("Toffu");
+            picker3.Items.Add("Capsicum");
+            picker3.Items.Add("Veg");
+            picker3.Items.Add("Pumpkin");
+            picker3.Items.Add("None");
 
-            var switcher = new Switch();
+            var picker4 = new Picker
+            {
+                Title = "Toping #3",
+                IsVisible = true //<= Is on the page but doesnt use space
+            };
+            picker4.Items.Add("Pepperoni");
+            picker4.Items.Add("Ham");
+            picker4.Items.Add("Mushroom");
+            picker4.Items.Add("Chillies");
+            picker4.Items.Add("Toffu");
+            picker4.Items.Add("Capsicum");
+            picker4.Items.Add("Veg");
+            picker4.Items.Add("Pumpkin");
+            picker4.Items.Add("None");
+
+            var pickerT = new Picker
+            {
+                Title = "Pizza Style",
+                IsVisible = true //<= Is on the page but doesnt use space
+            };
+            pickerT.Items.Add("Custome");
+            pickerT.Items.Add("Pepperoni");
+            pickerT.Items.Add("Hawaian");
+            pickerT.Items.Add("Portobello");
+            pickerT.Items.Add("Meat Feast");
+            pickerT.Items.Add("Spicy Chicken");
+            pickerT.Items.Add("Pumpkin");
+            pickerT.Items.Add("Four Cheese");
+         
+
+
+
+
+
+
 
             var listView = new ListView
             {
                 ItemsSource = _sqLiteConnection.Table<TodoItem>()
             };
 
+            try
+            {
+                var pickerSelection = picker.Items[picker.SelectedIndex];
+            } catch(Exception e)
+            {
+                //picker error
+            }
+            try
+            {
+                var pickerSelection2 = picker2.Items[picker.SelectedIndex];
+            }
+            catch (Exception e)
+            {
+                //picker error
+            }
+            try
+            {
+                var pickerSelection3 = picker3.Items[picker.SelectedIndex];
+            }
+            catch (Exception e)
+            {
+                //picker error
+            }
+            try
+            {
+                var pickerSelection4 = picker4.Items[picker.SelectedIndex];
+            }
+            catch (Exception e)
+            {
+                //picker error
+            }
+
+            try
+            {
+                var pickerSelection5 = pickerT.Items[picker.SelectedIndex];
+            }
+            catch (Exception e)
+            {
+                //picker error
+            }
+
             var addButton = new Button
             {
-                Text = "Add TodoItem"
+                Text = "Add Pizza Type"
             };
             addButton.Clicked += (s, e) =>
             {
+                var pickerSelection = picker.Items[picker.SelectedIndex];
+                var pickerSelection2 = picker.Items[picker.SelectedIndex];
+                var pickerSelection3 = picker.Items[picker.SelectedIndex];
+                var pickerSelection4 = picker.Items[picker.SelectedIndex];
+                var pickerSelection5 = pickerT.Items[picker.SelectedIndex];
                 _sqLiteConnection.Insert(new TodoItem
                 {
-                    Text = PizzaName.Text,
-                    Done = switcher.IsToggled,
+                    PizzaName = pickerSelection5,
+                    Type = pickerSelection,
+                    Topping1 = pickerSelection2,
+                    Topping2 = pickerSelection3,
+                    Topping3 = pickerSelection4,
+                    
                 });
                 listView.ItemsSource = _sqLiteConnection.Table<TodoItem>();
             };
 
+           var SummaryButton = new Button
+           {
+                Text = "Summary"
+           };
+            SummaryButton.Clicked += (s, e) =>
+            {
+                
+                };
+                // _sqLiteConnection.Insert(new TodoItem
+                {
+
+                }
+            
+
+          
+
+
            
-
-            var refreshButton = new Button
-            {
-                Text = "Refresh TodoItems"
-            };
-
-
-            refreshButton.Clicked += (s, e) =>
-            {
-                listView.ItemsSource = _sqLiteConnection.Table<TodoItem>();
-            };
-
             var contentPage = new ContentPage
             {
                 Content = new StackLayout
@@ -119,14 +208,27 @@ namespace LAPIZZA2
                             Children =
                             {
                                 picker,
-                                PizzaName,
-                                switcher,
+                               
+
                             }
                         },
-                        PizzaTopping,
+                        new StackLayout
+                        {
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                
+                                pickerT,
+                                
+                            }
+                        },
+                        picker2,
+                        picker3,
+                        picker4,
                         addButton,
-                        refreshButton,
                         listView,
+
+
                     }
                 }
             };
--- LAPIZZA2/SQLite-AppSample/TodoItem.cs
+++ LAPIZZA2/SQLite-AppSample/TodoItem.cs
@@ -8,13 +8,21 @@ namespace XamarinForms.SQLite
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
 
-        public string Text { get; set; }
+        public string PizzaName { get; set; }
+
+        public string Topping1 { get; set; }
+
+        public string Topping2 { get; set; }
+
+        public string Topping3 { get; set; }
+
+        public string Type { get; set; }
 
         public bool Done { get; set; }
 
         public override string ToString()
         {
-            return string.Format("Done : {0}, Text : {1}", Done, Text);
+            return string.Format("Done : {0}, Type : {1},  PizzaName : {2}, Topping1 : {3}, Topping2 : {4}, Topping3 : {5}", Done, Type, PizzaName, Topping1, Topping2, Topping3);
         }
     }
 }
